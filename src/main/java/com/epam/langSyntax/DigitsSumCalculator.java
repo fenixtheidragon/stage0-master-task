87130c1284991ce62b0c5f27dd18737e4e9f0a2c
fenix <fenixtheidragon@gmail.com>
2023-10-02 14:12:06 +0300

    Lang Syntax

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -1,8 +1,16 @@
 package com.epam.langSyntax;
-
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int numberCopy = number;
+        int sum = 0;
+        do {
+            int remainder = numberCopy % 10;
+            sum += remainder;
+            numberCopy /= 10;
+        } while (numberCopy % 10 != 0);
+
+        System.out.println(sum);
     }
 
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,15 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        String numberStr = String.valueOf(number);
+        String[] digits = numberStr.split("");
+        StringBuilder reversedNumberSB = new StringBuilder();
+        for (int a = digits.length - 1; a >= 0; a--) {
+            reversedNumberSB.append(digits[a]);
+        }
+        String reversedNumberStr = reversedNumberSB.toString();
+        int reversedNumber = Integer.parseInt(reversedNumberStr);
+        System.out.println(reversedNumber);
     }
 
 }
