78fb5e488ffbea22704e6f035451c6410ba58d27
fenix <fenixtheidragon@gmail.com>
2023-10-02 14:33:19 +0300

    Conditions

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,26 @@
 package com.epam.conditions;
 
 public class DaysInMonth {
-
     public void printDays(int year, int month) {
-
+        boolean leapOrNot = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        String result;
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                result = "31";
+                break;
+            case 4, 6, 9, 11:
+                result = "30";
+                break;
+            case 2:
+                if (leapOrNot) {
+                    result = "29";
+                } else {
+                    result = "28";
+                }
+                break;
+            default:
+                result = "invalid date";
+        }
+        System.out.println(result);
     }
-
 }
--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -3,6 +3,13 @@ package com.epam.conditions;
 public class SeasonDeterminer {
 
     public void tellTheSeason(int monthNumber) {
+        String result = switch (monthNumber) {
+            case 1, 2, 12 -> "Winter";
+            case 3, 4, 5 -> "Spring";
+            case 6, 7, 8 -> "Summer";
+            case 9, 10, 11 -> "Autumn";
+            default -> "Wrong month number";
+        };
+        System.out.println(result);
     }
-
 }
